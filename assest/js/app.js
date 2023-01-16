abe492ea0189660051e8910924540a5de2569f57
hagar hassan <hagarhassan813@gmail.com>
2023-01-17 00:53:23 +0200

    add navbar using js

--- assest/js/app.js
+++ assest/js/app.js
@@ -1,34 +1,30 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
+// define global variable 
 
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
-*/
+/* **************** start define navbar variable ***************/ 
+var liList;
+var aLi;
+var itemListDiv;
+var ulList;
+// create div have ul 
+itemListDiv = document.createElement("div");
+itemListDiv.setAttribute("id","navbarNav");
+// create ul element
+ulList = document.createElement("ul");
+ulList.classList.add("ulClass");
+// create array to store links have key and values 
+var liArray = [
+    "Home", "gallery","about","contact"
+];
 
-/**
- * Define Global Variables
- * 
-*/
+/*********************** end define navbar variable**********************************/
 
 
-/**
- * End Global Variables
+/*
  * Start Helper Functions
  * 
 */
+//  function to add hover in li 
+
 
 
 
@@ -38,8 +34,38 @@
  * 
 */
 
-// build the nav
+/**********************  build the nav ********************** */
 
+// append div hav ul into nav in header 
+document.getElementById("navbar").appendChild(itemListDiv);
+// to create list of li we have need create for loop 
+for( let i = 0 ; i < liArray.length; i++ ){
+    liList = document.createElement("li"); // create li list 
+    liList.classList.add("liClass");
+   
+    aLi = document.createElement("a"); // create link element 
+    aLi.classList.add("linkClass");
+    aLi.setAttribute("href","#"+ liArray[i]); // add attribute href to link of section 
+    aLi.innerHTML= liArray[i]; // add array to html 
+    liList.appendChild(aLi); 
+    ulList.appendChild(liList);
+    console.log(liList);
+
+    //  function to add hover in li 
+    aLi.addEventListener("mouseover", onHover);
+    function onHover(e){
+        e.target.classList.add("linkHover");
+    };
+
+    // function remove hover on li 
+    aLi.addEventListener("mouseout", outHover);
+    function outHover(e){
+        e.target.classList.remove("linkHover");
+    };
+
+}
+itemListDiv.appendChild(ulList); // append ul to div in nav 
+/******************* end navbar ************************/
 
 // Add class 'active' to section when near top of viewport
 
